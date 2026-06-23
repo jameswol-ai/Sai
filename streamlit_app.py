c3972a11c5eb3e79f0928a1b44f8146df18ebb84
James Wol <james.wol@petalmail.com>
2026-06-24 00:19:36 +0300

    Remove import of run_trading_loop from streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import threading
 import logging
-from trading_loop import run_trading_loop
 from plugins import risk_plugins, notifier_plugins, strategy_plugins
 
 # Configure logging
